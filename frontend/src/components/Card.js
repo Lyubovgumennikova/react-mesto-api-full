d2dc26055c57c955a0cb03ff026d4a452d569786
Lyubovgumennikova <yubovgumennikova@yandex.ru>
2022-04-15 03:23:43 +0300

    mesto15

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -3,7 +3,7 @@ import { CurrentUserContext } from "../contexts/CurrentUserContext";
 
 function Card({ card, onCardClick, onCardLike, onCardDelete }) {
   const currentUser = React.useContext(CurrentUserContext);
-  const isOwn = card.owner._id === currentUser._id;
+  const isOwn = card.owner === currentUser._id;
   // Создаём переменную, которую после зададим в `className` для кнопки удаления
   const cardDeleteButtonClassName = `element__remove-button ${
     isOwn ? "element__remove-button_active" : "element__remove-button_hidden"
